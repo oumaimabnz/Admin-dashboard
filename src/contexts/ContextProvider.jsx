3e0b496a6f7ddf6f133cda232ef740f7001b9d2e
oumaimabnz <oumaimabenaziza21@gmail.com>
2026-01-14 18:16:04 +0100

    Initial commit

---
+++ src/contexts/ContextProvider.jsx
@@ -0,0 +1,65 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const StateContext = createContext()
+
+const initialState = {
+    chat:false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu ] = useState(true)
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+    const [currentColor, setCurrentColor] = useState('#03C9D7')
+    const [currentMode, setCurrentMode] = useState('Light')
+    const [themeSettings, setThemeSettings] = useState(false)
+
+    
+    useEffect(() => {
+        console.log("Mode changed to:", currentMode);
+         if (currentMode === "Dark") {
+           document.documentElement.classList.add("dark");
+         } else {
+           document.documentElement.classList.remove("dark");
+         }
+       }, [currentMode]);
+
+
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value)
+        setThemeSettings(false)
+    }
+
+    
+    const setColor = (mode) => {
+        setCurrentColor(mode);
+        localStorage.setItem('colorMode', mode)
+        setThemeSettings(false)
+    }
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState,[clicked]: true})
+    }
+
+    return (
+        <StateContext.Provider 
+           value={{activeMenu, setActiveMenu, 
+           isClicked, setIsClicked, 
+           handleClick, 
+           screenSize, setScreenSize, 
+           currentColor, currentMode, 
+           themeSettings, setThemeSettings,
+           setMode, setColor
+        }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
